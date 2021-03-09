3c8ed9272cfa31e56478afc23305b2b67de73396
Rafael Camarda <rf.camarda@gmail.com>
2021-03-09 11:36:24 -0300

    project setup and first route

---
+++ lib/rockelivery_web/controllers/welcome_controller.ex
@@ -0,0 +1,10 @@
+defmodule RockeliveryWeb.WelcomeController do
+  use RockeliveryWeb, :controller
+
+  def index(conn, _params) do
+    conn
+    |> put_status(:ok)
+    |> text("Welcome!")
+    |> IO.inspect()
+  end
+end
