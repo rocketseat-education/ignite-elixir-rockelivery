01342479601119a08e170df93825ea74051e431f
Rafael Camarda <rf.camarda@gmail.com>
2021-03-31 15:12:59 -0300

    creates report runner

---
+++ lib/rockelivery/orders/report_runner.ex
@@ -0,0 +1,37 @@
+defmodule Rockelivery.Orders.ReportRunner do
+  use GenServer
+
+  require Logger
+
+  alias Rockelivery.Orders.Report
+
+  # Client
+
+  def start_link(_initial_state) do
+    GenServer.start_link(__MODULE__, %{})
+  end
+
+  # SERVER
+
+  @impl true
+  def init(state) do
+    Logger.info("Report Runner started")
+    schedule_report_generation()
+
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_info(:generate, state) do
+    Logger.info("Generating report...")
+
+    Report.create()
+    schedule_report_generation()
+
+    {:noreply, state}
+  end
+
+  def schedule_report_generation do
+    Process.send_after(self(), :generate, 1000 * 10)
+  end
+end
---
+++ lib/rockelivery/stack.ex
@@ -0,0 +1,47 @@
+defmodule Rockelivery.Stack do
+  use GenServer
+
+  # Client
+
+  def start_link(initial_stack) when is_list(initial_stack) do
+    GenServer.start_link(__MODULE__, initial_stack)
+  end
+
+  def push(pid, element) do
+    GenServer.cast(pid, {:push, element})
+  end
+
+  def pop(pid) do
+    GenServer.call(pid, :pop)
+  end
+
+  # Server (Callbacks)
+
+  @impl true
+  def init(stack) do
+    {:ok, stack}
+  end
+
+  @impl true
+  # SYNC
+  def handle_call({:push, element}, _from, stack) do
+    new_stack = [element | stack]
+    {:reply, new_stack, new_stack}
+  end
+
+  @impl true
+  def handle_call(:pop, _from, [head | tail]) do
+    {:reply, head, tail}
+  end
+
+  @impl true
+  def handle_call(:pop, _from, []) do
+    {:reply, nil, []}
+  end
+
+  @impl true
+  # ASYNC
+  def handle_cast({:push, element}, stack) do
+    {:noreply, [element | stack]}
+  end
+end
